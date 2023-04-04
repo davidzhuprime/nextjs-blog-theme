672a02a03452ae18b68e6f3da96bc81aaec32c6c
Zhu, Lin <lin_zhu8@dell.com>
2023-04-04 21:25:25 +0800

    dddd

--- pages/api/kafka.js
+++ pages/api/kafka.js
@@ -1,8 +1,27 @@
-// pages/api/kafka.js
-import { Kafka } from 'kafkajs';
+const { Kafka } = require('kafkajs')
 
-export default async function handler(req, res) {
-  // Your 'kafkajs' logic goes here
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
 
-  res.status(200).json({ message: 'Your response data' });
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'my-topic', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const messageContent = message.value.toString()
+      // Process the message here
+    },
+  })
 }
+export default async function handler(req, res) {
+    // Your 'kafkajs' logic goes here
+  
+    res.status(200).json({ message: 'Your response data' });
+  }
+  
+run().catch(console.error)
